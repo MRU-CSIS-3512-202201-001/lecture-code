d6ad3efe7310f571474c19075e442e41183af96c
jpratt-mru <jpratt@mtroyal.ca>
2022-01-20 11:13:03 -0700

    Push before lec-04 starts.

---
+++ lec-04/04-map/start/index.js
@@ -0,0 +1,39 @@
+let boringWords = ["sigh", "droop", "laze"];
+
+// 🧠 What if we want to transform them into CAPITALIZED VERSIONS.
+//    !!!!WITH EXCLAMATION MARKS!!!!
+
+function excitify(word) {
+  // 🧠 Can you do this with a string template? Try!
+}
+
+// ------------------------------------------
+
+const photos = [
+  {
+    title: "a droopy flower",
+    location: {
+      lat: 12,
+      lon: 53,
+    },
+    photographer: "Ben Miels",
+  },
+  {
+    title: "lazy day in the drain",
+    location: {
+      lat: -44,
+      lon: 0,
+    },
+    photographer: "Roger Foreman",
+  },
+  {
+    title: "sticky goo on my shoe",
+    location: {
+      lat: 31,
+      lon: 135,
+    },
+    photographer: "Ben Miels",
+  },
+];
+
+// 🧠 What if we just want an array of the titles?
---
+++ lec-04/05-sort/start/index.js
@@ -0,0 +1,45 @@
+// 🧠 How could we sort these?
+const colourNames = [
+  "Red",
+  "Orange",
+  "Yellow",
+  "Green",
+  "Blue",
+  "Indigo",
+  "Violet",
+  "Unicorn",
+];
+
+// 🧠 How could we sort these?
+const shoeSizes = [8, 12, 6.5, 11, 16, 9, 8];
+
+const adventurers = [
+  {
+    name: "Jampa",
+    class: "Barbarian",
+    level: 5,
+    hp: 74,
+    ac: 4,
+  },
+  {
+    name: "Flame of the Evading Desert",
+    class: "Rogue",
+    level: 6,
+    hp: 52,
+    ac: 2,
+  },
+  {
+    name: "Zog Mothra",
+    class: "Wizard",
+    level: 3,
+    hp: 11,
+    ac: 8,
+  },
+  {
+    name: "Jassan",
+    class: "Wizard",
+    level: 5,
+    hp: 23,
+    ac: 7,
+  },
+];
