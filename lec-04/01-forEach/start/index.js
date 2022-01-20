b1148c656ac0581b23be84114341b723af36720b
jpratt-mru <jpratt@mtroyal.ca>
2022-01-20 06:13:05 -0700

    Some lec-04 stuff.

---
+++ lec-04/01-forEach/start/index.js
@@ -0,0 +1,22 @@
+const nums = [1, 2, 3, 4, 5, 6];
+
+function log(something) {
+  console.log(something);
+}
+
+// nums.forEach(log);
+
+// 🧠 What would this look like with an arrow function?
+
+// HOTEL CALIFORNIA
+// Say you're the kind of person who likes to break out of a for....
+
+const otherNums = [3, 5, -1, 17, 0];
+
+otherNums.forEach((num) => {
+  if (num == -1) {
+    break;
+  } else {
+    console.log(num);
+  }
+});
