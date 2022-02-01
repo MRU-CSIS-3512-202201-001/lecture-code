980f5cceb7d56a20b8593533f4c0f609c353285e
jpratt-mru <jpratt@mtroyal.ca>
2022-02-01 09:04:41 -0700

    Another

---
+++ lec-07/05-event-delegation/index.js
@@ -0,0 +1,23 @@
+// Listen at various places
+// Notice where the script tag is.
+
+document.addEventListener("DOMContentLoaded", () => {
+  // 🧠 Notice anything...interesting...about the button tags?
+  // 🧠 What are those things?!?
+  // 🧠 How do you get those values via JS?
+  //
+  // Keep this stuff in mind. We're gonna need it.
+  //
+  // 🧠 What if we wanted each button to respond to a click and log its
+  //    nickname to console?
+  // 🧠 What's one problem with this? Hint: what if there were 100 buttons?
+  //
+  // Build the same behaviour, but with event delegation:
+  //  - try without conditional...uh-oh, Sally Mae, you rascal
+  //  - use the debugger and remember when you're paused you can examine variables
+  //
+  // Does any of this ring a bell for the assignment?
+  // (It will if you've been bored and read the pdf carefully...but I get it.)
+  //
+  //
+});
