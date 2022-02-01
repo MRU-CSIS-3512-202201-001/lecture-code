6a72a7c46dabdf64115b215833378604b621a115
jpratt-mru <jpratt@mtroyal.ca>
2022-02-01 10:59:19 -0700

    Copy starting

---
+++ lec-07/starting/01-clicking-a-div/index.js
@@ -0,0 +1,2 @@
+// write code that changes the text of the div when it is clicked
+// notice where we've put our script tag
---
+++ lec-07/starting/02-many-divs/index.js
@@ -0,0 +1,2 @@
+// write code that changes the text EVERY div when it is clicked
+// notice where we've put our script tag
