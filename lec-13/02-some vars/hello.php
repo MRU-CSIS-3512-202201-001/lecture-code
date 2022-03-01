bcf69b7f7690b2a5be33d63a36c11ef6be20fd51
jpratt-mru <jpratt@mtroyal.ca>
2022-03-01 05:08:10 -0700

    Add some codez.

---
+++ lec-13/02-some vars/hello.php
@@ -0,0 +1,10 @@
+<?php
+
+$greeting = "hello, crazyworld";
+echo $greeting;
+
+// ✔️ So yeah...variables start with a $ ... that's prolly gonna drive you bonkers
+//    for a while.
+
+$greeting = 123;  // 🧠 Can we do this?!?! How is this similar to JS?
+echo $greeting;   // 🧠 What does our page look like now?
