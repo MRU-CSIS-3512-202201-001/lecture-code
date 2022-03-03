aede93e465252327f9d4763b19d661fd76663874
jpratt-mru <jpratt@mtroyal.ca>
2022-03-03 10:55:36 -0700

    lec-14 done

---
+++ lec-14/04-hyperlinks/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="cheese-details.php?name=cottage&price=1.30">cottage cheese</a></li>
+        <li><a href="cheese-details.php?name=brie&price=4.80">Brie</a></li>
+        <li><a href="cheese-details.php?name=Crottin+de+Chavignol&price=11.10">Crottin de Chavignol</a></li>
+    </ul>
+</body>
+
+</html>
---
+++ lec-14/starting/02-array-iteration/iteration.php
@@ -0,0 +1,19 @@
+<?php
+
+// let's make an array
+$usernames = ['squidz', "r@nd0r", "NUX_BL@5TER"];
+
+// 🧠 How could we display this as an unordered list?
+//    Use a function.
+
+
+
+
+$movie = [
+    "title" => "Beetlejuice",
+    "year" => 1980,
+    "director" => "Tim Burton"
+];
+
+// 🧠 How could we display this as field: value divs inside another div?
+//    Use a function.
