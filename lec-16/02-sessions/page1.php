af253ed457d81ceda745d449b7bbdf82bde1e0c4
jpratt-mru <jpratt@mtroyal.ca>
2022-03-10 11:08:07 -0700

    Lec-16

---
+++ lec-16/02-sessions/page1.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+// 🧠 What did this do?
+// 🧠 How can we put things in here?
+// 🧠 How can we get things out of here?
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page One</title>
+</head>
+
+<body>
+    <h1>Page One - I'm Looking In Session Storage</h1>
+    <a href="page2.php">to page 2</a>
+
+</body>
+
+</html>
---
+++ lec-16/starting/00-some-hints/some-other-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Some Other Page</title>
+</head>
+
+<body>
+
+    <header>
+        <nav>
+
+            <ul>
+                <li><a href="index.php">index</a></li>
+                <li><a href="some-page.php">some page</a></li>
+                <li><a href="some-other-page.php">some other page</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="content">
+        <div>
+            Some other content
+        </div>
+    </div>
+
+</body>
+
+</html>
